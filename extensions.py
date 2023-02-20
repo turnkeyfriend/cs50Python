44e3d379059f7c848683ea7e193b0e0bd3b6c079
turnkeyfriend <rmc_luke@netzero.net>
2023-02-20 17:25:22 -0500

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,20 @@
+filename = input("Please enter a filename: ")
+
+filename = filename.strip().lower()
+
+if filename.endswith(".gif"):
+    print("image/gif")
+elif filename.endswith(".jpg"):
+    print("image/jpeg")
+elif filename.endswith(".jpeg"):
+    print("image/jpeg")
+elif filename.endswith(".png"):
+    print("image/png")
+elif filename.endswith(".pdf"):
+    print("application/pdf")
+elif filename.endswith(".txt"):
+    print("text/plain")
+elif filename.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
