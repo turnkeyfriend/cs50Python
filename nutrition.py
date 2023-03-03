93ca7f4bfaa05a3a481a47cb415a67bf1a191e8c
turnkeyfriend <rmc_luke@netzero.net>
2023-03-03 02:06:40 -0500

    Create nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,29 @@
+fruits = {
+    "Apple": "130",
+    "Avocado": "50",
+    "Banana": "110",
+    "Cantaloupe": "50",
+    "Grapefruit": "60",
+    "Grapes": "90",
+    "Honeydew": "50",
+    "Kiwifruit": "90",
+    "Lemon": "15",
+    "Lime": "20",
+    "Nectarine": "60",
+    "Orange": "80",
+    "Peach": "60",
+    "Pear": "100",
+    "Pineapple": "50",
+    "Plums": "70",
+    "Strawberries": "50",
+    "Sweet Cherries": "100",
+    "Tangerine": "50",
+    "Watermelon": "80",
+}
+
+fruit_input = str.title(input("Item: "))
+
+try:
+    print(f"Calories: {fruits[fruit_input]}")
+except KeyError:
+    print()
